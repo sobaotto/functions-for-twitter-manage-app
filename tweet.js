58672fc3479ef1e6f0fa54426a6ee36d20ff52b3
Your Name <you@example.com>
2021-03-19 20:19:23 +0900

    add comment

--- tweet.js
+++ tweet.js
@@ -1,6 +1,7 @@
 const Twitter = require("twitter");
 const oAuth = require("./keys");
 
+// 外部モジュールを用いて、認証を行い、ツイートができる状態の関数を作る
 const tweet = (postContent) => {
   const client = new Twitter({ ...oAuth });
 
